958257b6acd8810442fc6e69b21e27e74c208e5c
Ryan Schmidt <git@ryandesign.com>
2021-07-20 17:36:33 -0500

    Fix typo: retuen -> return

--- uninstall.py
+++ uninstall.py
@@ -25,7 +25,7 @@ def usage(exitCode=2):
     """Display usage info and exit.
 
     Arguments:
-        exitCode -- the code to retuen when exiting.
+        exitCode -- the code to return when exiting.
     """
     global prefixDir
     print('Usage:')
